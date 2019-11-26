a2f19259a803319b9b8893100b6854edb587880d
bruce <brucec@hotmail.com>
2019-11-26 13:34:08 -0800

    add methods for garbage

--- A2Code/Garbage.h
+++ A2Code/Garbage.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <iostream>
 class Garbage
 {
+private:
+	void write(std::string message) {
+		std::cout << "Garbage: " << message << std::endl;
+	}
+public:
+	void recycle(bool filter) {
+		write("filter disposed");
+	}
+
 };
 
