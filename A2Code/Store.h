c5bfc42335bef7ddf17742ca84848650dc628fba
bruce <brucec@hotmail.com>
2019-11-26 13:31:00 -0800

    update methods for store

--- A2Code/Store.h
+++ A2Code/Store.h
@@ -1,5 +1,27 @@
 #pragma once
+#include "Oil.h"
+#include "TireBatch.h"
+#include <iostream>
 class Store
 {
+public:
+	Oil getOil(std::string type, int quantity) {
+		Oil to_return{ type,quantity };
+		return to_return;
+	}
+
+	bool getAirFilter() {
+		return true;
+	}
+
+	bool getOilFilter() {
+		return true;
+	}
+
+	TireBatch getTires(std::string make, int size, int quantity) {
+		TireBatch to_return{ make,size,quantity };
+		return to_return;
+	}
+	
 };
 
