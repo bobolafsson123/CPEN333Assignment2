716a466cbeef0d480b31914d7ad07105edee2e8e
bruce <brucec@hotmail.com>
2019-11-26 13:33:05 -0800

    update methods for recycling

--- A2Code/Recycling.h
+++ A2Code/Recycling.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "Oil.h"
+#include "TireBatch.h"
 class Recycling
 {
+private:
+	void write(std::string message) {
+		std::cout << "Recycling: " << message << std::endl;
+	}
+public:
+	void recycle(Oil oldOil) {
+		write("oldOil disposed");
+	}
+	void recycle(TireBatch tireBatch) {
+		write("old tireBatch disposed");
+	}
 };
 
